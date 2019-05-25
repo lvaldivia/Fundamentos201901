40530d9abacdf2aad4692fe86f68eb91cc830167
wilfredo morales <wmorales83@gmail.com>
2019-05-24 19:24:32 -0500

    spritenatch v2

--- Proyectos/PapuEngine/SpriteBatch.cpp
+++ Proyectos/PapuEngine/SpriteBatch.cpp
@@ -1,12 +1,144 @@
 #include "SpriteBatch.h"
+#include <algorithm>
 
-
-
-SpriteBatch::SpriteBatch()
+SpriteBatch::SpriteBatch() :vbo(0), vao(0)
 {
+}
+
+void SpriteBatch::init() {
+	createVertexArray();
+}
+
+void SpriteBatch::begin(GlypSortType sortType) {
+	sortType = sortType;
+	renderbatches.clear();
+	glyphs.clear();
+}
+void SpriteBatch::end() {
+	glyphsPointers.resize(glyphs.size());
+	for (size_t i = 0; i < glyphs.size(); i++)
+	{
+		glyphsPointers[i] = &glyphs[i];
+	}
+	sortGlyph();
+	createRenderBatches();
+}
+
+void SpriteBatch::createRenderBatches() {
+	std::vector<Vertex> vertices;
+	vertices.resize(glyphs.size() * 6);
+	if (glyphs.empty()) {
+		return;
+	}
+	int cv = 0;
+	int ofset = 0;
+	renderbatches.emplace_back(ofset, 6, glyphsPointers[0]->texture);
+	vertices[cv++] = glyphsPointers[0]->topLeft;
+	vertices[cv++] = glyphsPointers[0]->bottomLeft;
+	vertices[cv++] = glyphsPointers[0]->bottomRight;
+	vertices[cv++] = glyphsPointers[0]->bottomRight;
+	vertices[cv++] = glyphsPointers[0]->topRight;
+	vertices[cv++] = glyphsPointers[0]->topLeft;
+	ofset += 6;
+
+
+	for (int cg = 1; cg < glyphsPointers.size(); cg++)
+	{
+		if (glyphsPointers[cg]->texture != glyphsPointers[cg - 1]->texture) {
+			renderbatches.emplace_back(ofset, 6, glyphsPointers[cg]->texture);
+		}
+		else {
+			renderbatches.back()._numVertices += 6;
+		}
+
+		vertices[cv++] = glyphsPointers[cg]->topLeft;
+		vertices[cv++] = glyphsPointers[cg]->bottomLeft;
+		vertices[cv++] = glyphsPointers[cg]->bottomRight;
+		vertices[cv++] = glyphsPointers[cg]->bottomRight;
+		vertices[cv++] = glyphsPointers[cg]->topRight;
+		vertices[cv++] = glyphsPointers[cg]->topLeft;
+		ofset += 6;
+
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex),vertices.data(), GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), nullptr, GL_DYNAMIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(Vertex), vertices.data());
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void SpriteBatch::renderBatch() {
+	glBindVertexArray(vao);
+	for (int i = 0; i < renderbatches.size(); i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, renderbatches[i]._texture);
+		glDrawArrays(GL_TRIANGLES, renderbatches[i]._offset, renderbatches[i]._numVertices);
+	}
+	glBindVertexArray(0);
+}
+
+void SpriteBatch::draw(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint texture, float depth, const Color& ColorRGBA) {
+	glyphs.emplace_back(destRect, uvRect, texture, depth, ColorRGBA);
+}
+
+void SpriteBatch::sortGlyph() {
+	switch (sortType)
+	{
+	case GlypSortType::BACK_TO_FRONT:
+		std::stable_sort(glyphsPointers.begin(), glyphsPointers.end(), compareBackToFront);
+		break;
+	case GlypSortType::FRONT_TO_BACK:
+		std::stable_sort(glyphsPointers.begin(), glyphsPointers.end(), compareFrontToBack);
+		break;
+	case GlypSortType::TEXTURE:
+		std::stable_sort(glyphsPointers.begin(), glyphsPointers.end(), compareTexture);
+		break;
+	}
+}
+
+bool SpriteBatch::compareFrontToBack(Glyph* a, Glyph* b) {
+	return (a->depth < b->depth);
+}
+
+bool SpriteBatch::compareBackToFront(Glyph* a, Glyph* b) {
+	return (a->depth > b->depth);
+}
+
+bool SpriteBatch::compareTexture(Glyph* a, Glyph* b) {
+	return (a->texture < b->texture);
+}
+
+void SpriteBatch::createVertexArray() {
+	if (vao == 0) {
+		glGenVertexArrays(1, &vao);
+	}
+	glBindVertexArray(vao);
+	if (vbo == 0) {
+		glGenBuffers(1, &vbo);
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+		(void*)offsetof(Vertex, position));
+
+	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex),
+		(void*)offsetof(Vertex, color));
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+		(void*)offsetof(Vertex, uv));
+	glBindVertexArray(0);
 }
 
 
 SpriteBatch::~SpriteBatch()
+{
+}
+Renderbatch::Renderbatch(GLuint offset, GLuint numVertixex, GLuint texture) :_offset(offset), _numVertices(numVertixex), _texture(texture)
 {
 }
--- Proyectos/PapuEngine/SpriteBatch.h
+++ Proyectos/PapuEngine/SpriteBatch.h
@@ -11,19 +11,40 @@ enum class GlypSortType {
 	TEXTURE
 };
 
-struct Glyph {
+class Glyph {
+public:
+	Glyph() {}
+	Glyph(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint _texture, float _depth, const Color& color) {
+		texture = _texture;
+		depth = _depth;
+
+		topLeft.color = color;
+		topLeft.setPosition(destRect.x, destRect.y + destRect.w);
+		topLeft.setUV(uvRect.x, uvRect.y + uvRect.w);
+
+		bottomLeft.color = color;
+		bottomLeft.setPosition(destRect.x, destRect.y);
+		bottomLeft.setUV(uvRect.x, uvRect.y);
+
+		bottomRight.color = color;
+		bottomRight.setPosition(destRect.x + destRect.z, destRect.y);
+		bottomRight.setUV(uvRect.x + uvRect.z, uvRect.y);
+
+		topRight.color = color;
+		topRight.setPosition(destRect.x + destRect.z, destRect.y + destRect.w);
+		topRight.setUV(uvRect.x + uvRect.z, uvRect.y + uvRect.w);
+	}
 	GLuint texture;
 	float depth;
 	Vertex topLeft;
 	Vertex bottomLeft;
 	Vertex topRight;
 	Vertex bottomRight;
-
 };
 
 class Renderbatch {
 public:
-	Renderbatch(GLuint _offset, GLuint _numVertices, GLuint _texture);
+	Renderbatch(GLuint offset, GLuint numVertixex, GLuint texture);
 	GLuint _offset;
 	GLuint _numVertices;
 	GLuint _texture;
@@ -35,8 +56,9 @@ class SpriteBatch
 private:
 	GLuint vbo;
 	GLuint vao;
-	std::vector<Glyph*> glyphs;
+	std::vector<Glyph> glyphs;
 	std::vector<Renderbatch> renderbatches;
+	std::vector<Glyph*> glyphsPointers;
 	void createVertexArray();
 	void sortGlyph();
 	void createRenderBatches();
@@ -44,10 +66,9 @@ public:
 	void init();
 	void begin(GlypSortType sortGlyph = GlypSortType::TEXTURE);
 	void end();
-	void draw(const glm::vec4& destRect, const glm::vec4& uvRect,
-		GLuint texture, float depth, Color& color
-	);
+	void draw(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint texture, float depth, const Color& ColorRGBA);
 	void renderBatch();
+	GlypSortType sortType;
 	static bool compareFrontToBack(Glyph* a, Glyph* b);
 	static bool compareBackToFront(Glyph* a, Glyph* b);
 	static bool compareTexture(Glyph* a, Glyph* b);
